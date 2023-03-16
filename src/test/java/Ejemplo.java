4a2fa148d3b16611727223afaacbc344ad563445
wtaque17 <taque0817@hotmail.com>
2023-03-15 21:48:28 -0500

    Clase 5

--- src/test/java/Ejemplo.java
+++ src/test/java/Ejemplo.java
@@ -2,20 +2,28 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import java.io.File;
 import java.io.IOException;
 
 import java.sql.Driver;
 import java.time.Duration;
+import java.util.Random;
 
 public class Ejemplo{
-    
+
     @Test
     public  void prueba() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","./src/test/resources/Driver/chromedriver");
+        double random_1 = (Math.random() * 2);
+        Random random = new Random();
+        int random_2 = random.nextInt(1,5);
+        System.out.println(random_2);
+        System.setProperty("webdriver.chrome.driver", "./src/test/resources/Driver/chromedriver");
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
         driver.manage().window().maximize();
@@ -51,13 +59,13 @@ public class Ejemplo{
 
         try {
             File file = new File("PruebaDoc.txt");
-            if (file.createNewFile()){//creacion del archivo
+            if (file.createNewFile()) {//creacion del archivo
                 System.out.println("Se creo el archivo");
-            }else{
+            } else {
                 System.out.println("ya existe un doc con ese nombre");
             }
 
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Ha ocurrido un error");
             e.printStackTrace();//el error que tre se imprime
         }
@@ -92,6 +100,22 @@ public class Ejemplo{
         WebElement day = driver.findElement(By.xpath("//td[contains(text(),'11')]"));
         day.click();
 
+        WebElement default_radio = driver.findElement(By.xpath("//input[@type='radio']"));
+        default_radio.click();
+
+        WebElement example_range = driver.findElement(By.xpath("//input[@class='form-range']"));
+
+        if (random_1 <= 1) {
+            for (int i = 0; i < random_2; i++) {
+                example_range.sendKeys(Keys.ARROW_LEFT);
+            }
+        }else{
+            for (int i = 0; i < random_2; i++) {
+                example_range.sendKeys(Keys.ARROW_RIGHT);
+        }
+
+    }
+        System.out.println(random_1);
 
 
 
@@ -111,13 +135,9 @@ public class Ejemplo{
 
 
 
-        //WebElement opcion_list = driver.findElement(By.xpath("//option[@value='San Francisco']"));
-        //opcion_list.click();
 
 
 
-        //WebElement return_index = driver.findElement(By.xpath("/html/body/main/div/form/div/div[1]/div/a"));
-        //return_index.click();
 
 
 
